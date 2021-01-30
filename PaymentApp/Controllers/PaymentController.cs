933dc528918fca6651de964c1c398ac2cd980a80
Samrat Alamgir <alamgir.samrat@outlook.com>
2021-01-31 02:47:23 +0600

    Logger configured

--- PaymentApp/Controllers/PaymentController.cs
+++ PaymentApp/Controllers/PaymentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using PaymentManager.Contracts;
 using PaymentManager.Requests;
 using PaymentManager.Responses;
@@ -13,10 +14,12 @@ namespace PaymentApp.Controllers
     [ApiController]
     public class PaymentController : ControllerBase
     {
+        private readonly ILogger<PaymentController> _logger;
         private readonly IPaymentService _paymentService;
 
-        public PaymentController(IPaymentService paymentService)
+        public PaymentController(ILogger<PaymentController> logger, IPaymentService paymentService)
         {
+            _logger = logger;
             _paymentService = paymentService;
         }
 
@@ -28,10 +31,13 @@ namespace PaymentApp.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<PaymentResponse>> GetPayment(Guid id)
         {
+            _logger.LogInformation($"GetPayment invoked with id {id}");
+
             var paymentDto = await _paymentService.GetPaymentByIdAsync(id);
 
             if (paymentDto == null)
             {
+                _logger.LogError($"Payment info not found for id {id}");
                 return NotFound();
             }
 
@@ -46,6 +52,8 @@ namespace PaymentApp.Controllers
         [HttpPost]
         public async Task<ActionResult<PaymentResponse>> MakePayment(MakePaymentRequest request)
         {
+            _logger.LogInformation("New payment requested");
+
             var payment = await _paymentService.MakePayment(request);
 
             return CreatedAtAction(nameof(GetPayment), new { id = payment.PaymentId }, payment);

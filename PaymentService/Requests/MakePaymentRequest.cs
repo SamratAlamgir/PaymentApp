e31957cfd09902b0451006138fb6a571f4040356
Samrat Alamgir <alamgir.samrat@outlook.com>
2021-01-31 15:55:41 +0600

    Dto validation added

--- PaymentService/Requests/MakePaymentRequest.cs
+++ PaymentService/Requests/MakePaymentRequest.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PaymentManager.Requests
 {
     public class MakePaymentRequest
     {
-        // TODO: Can be taken from token
         public Guid MerchantId { get; set; }
+        [Required]
+        [MinLength(13)]
         public string CardNumber { get; set; }
+        [Required]
         public string ExpiryDate { get; set; }
+        [Required]
+        [Range(0.01, 99999)]
         public decimal Amount { get; set; }
+        [Required]
         public string CurrencyCode {get; set;}
+        [Required]
         public string CVV { get; set; }
     }
 }
